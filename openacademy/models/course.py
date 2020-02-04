c9b1e417470ba3d78b6c1e6126024b09d72b560d
Ali Alkaheli <aal@odoo.com>
2020-02-04 18:28:33 +0000

    [ADD]

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models #, fields, api
 
 
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
-#     _description = 'openacademy.openacademy'
+class openacademy(models.Model):
+    #create data in the datatbase which will be name:
+    _name = 'openacademy.openacademy'
+    _description = 'openacademy.openacademy'
 
 #     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
-#
+
 #     @api.depends('value')
 #     def _value_pc(self):
 #         for record in self:
