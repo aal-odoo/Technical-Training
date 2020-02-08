f87533c0b4529e28720019ff3f2c1a40e0af6099
Ali Alkaheli <aal@odoo.com>
2020-02-08 23:13:52 +0000

    update

--- forum/models/.ipynb_checkpoints/school-checkpoint.py
+++ forum/models/.ipynb_checkpoints/school-checkpoint.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
-
+from odoo import models,fields
 
 class ForumSchool(models.Model):
     _name = 'forum.school'
     _description = 'Forum School'
     _order = 'id, created_on desc'
-
-    name = fields.Char(string='School Name', required=True, index=True, help='Enter your Schol name')
-    address = fields.Html(string='Address')
-    responsible_id = fields.Many2one(comodel_name='res.users', required=True,
-                        string='Responsible', ondelete='restrict', copy=False)
-    created_on = fields.Date(string='Created On', required=True)
     
+    name = fields.Char(string="School Name", required=True, index=True, help='Enter your school name')
+    address = fields.Html(string="Address")
+    responsible_id = fields.Many2one(comodel_name='res.users', required=True, string='Responsible', ondelete='restrict', copy=False)
+    created_on = fields.Date(string='Created On', required=True)
